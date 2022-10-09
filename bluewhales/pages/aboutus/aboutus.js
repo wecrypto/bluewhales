423bbb5b8314a6504e0f78b007bb7fcaad726dce
jiaxilong <38203146+wecrypto@users.noreply.github.com>
2022-10-09 14:39:29 +0800

    Add files via upload
    version1.0.0

---
+++ bluewhales/pages/aboutus/aboutus.js
@@ -0,0 +1,20 @@
+const app = getApp()
+const cloudFileBase = "cloud://fanyi.6661-fanyi-1302120986"
+Page({
+  data: {
+    // 第一次进入的开屏动画
+    last_pos: 0,
+    swiperImgs: [
+      
+      cloudFileBase + "/images/jieshao.png",
+    ]
+  },
+  bindchange: function(e){
+    if(this.data.last_pos == this.data.swiperImgs.length-1 && e.detail.current == 0){
+      wx.switchTab({
+        url: '../index/index'
+      })
+    }
+    this.data.last_pos = e.detail.current;
+  }
+})
